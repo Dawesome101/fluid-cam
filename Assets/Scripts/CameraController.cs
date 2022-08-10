f4d004782ba790d805aa2e1a8b9b5ff6df667e9c
Dawesome101 <dawesome@danawesome.com>
2022-08-09 20:56:35 -0700

    Made adaptions to the camera controller so that it is more generic.  The intention is that when imported into a new project it is less specific about settings making it easier to adapt to a new project.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,9 +10,25 @@ namespace SolidSky
         public LookMode lookMode;
 
         private HoverController hoverController;
+
         private Collider hoverPlayerCollider;
+
+        //[Tooltip("This will automatically be set to the CameraAxis gameobject which is assigned at runtime. " + 
+        //    "However if you would like to manually retarget the camera axis to a different game object, " +
+        //    "set this to that object. Keep in mind the function of this is to serve as a 'desired' " + 
+        //    "rotation and position for the camera itself. The camera will always try to match this " + 
+        //    "rotation and position based on the dampers and other variables settings.")]
         public Transform camAxis;
-        private Transform camCenter;
+        //[Tooltip("This will automatically be set to at runtime. This is the hinge around which the camera " + 
+        //    "will orbit. It is typically set to an empty game object at the center of where the camera " + 
+        //    "should orbit, such as a players head, or the center of a vehicle.")]
+        public Transform camOrbitAxis;
+
+        [Tooltip("Set this to the object you would like the camera focus to orbit around. " +
+            "This would typically be something like the players head or the cockpit of a v" +
+            "ehicle, however it can be any transform so long as it is the object intended " +
+            "for the camera to orbit.")]
+        public Transform cameraOrbitPosition;
 
         public bool invertMouse;
         public bool invertGamepad;
@@ -31,6 +47,10 @@ namespace SolidSky
         public float camBumpLength = 5f;
         public float camHeight = 2f;
         public float camBumpHeight = 1f;
+
+        [Tooltip("Make sure to remove all layers from this mask that you do not want the " +
+            "camera bumper ray to collide with. Typically this mask should only include t" +
+            "he default layer but you may include any layers you wish.")]
         public LayerMask hoverPlayerLayerMask;
 
         private void Awake()
@@ -49,16 +69,29 @@ namespace SolidSky
 
             if (!GameObject.FindGameObjectWithTag("CameraAxis"))
             {
-                Debug.LogError("Please add an empty GameObject with the tag 'CameraAxis' to the scene.");
+                Debug.LogError("The camera axis object is missing from the scene. Create an " +
+                    "empty GameObject with the tag 'CameraAxis' then add it as a child of th" +
+                    "e camera that contains this script.");
             } 
             else camAxis = GameObject.FindGameObjectWithTag("CameraAxis").transform;
 
-            if (!GameObject.FindGameObjectWithTag("CameraAxisCenter"))
+            if (!GameObject.FindGameObjectWithTag("CameraOrbitAxis"))
             {
-                Debug.LogError("Please tag an object desired as the centerpoint for the camera hinge with the tag 'CameraAxisCenter'.");
-
+                Debug.LogError("The camera orbit axis object is missing from the scene. Create an " +
+                    "empty GameObject with the tag 'CameraOrbitAxis' then add it as a child of th" +
+                    "e camera that contains this script.");
             } 
-            else camCenter = GameObject.FindGameObjectWithTag("CameraAxisCenter").transform;
+            else camOrbitAxis = GameObject.FindGameObjectWithTag("CameraOrbitAxis").transform;
+
+            if (!cameraOrbitPosition)
+            {
+                Debug.LogError("The cameras orbit position has not been set.  Read the tooltip for cameraOrbitPosition in the inspector and follow the instructions.");
+            }
+            else { 
+                camOrbitAxis.parent = cameraOrbitPosition.transform;
+                camOrbitAxis.localPosition = Vector3.zero;
+                camOrbitAxis.localRotation = Quaternion.identity;
+            }
             
             transform.parent = null;
             camAxis.transform.parent = null;
@@ -90,6 +123,8 @@ namespace SolidSky
             SetCamAxisPosRot();
 
             SetCamPosRot();
+
+            //GetBumpPos();
         }
 
         private void FixedUpdate()
@@ -100,9 +135,9 @@ namespace SolidSky
         public bool lookModeTransitionActive;
         private void SetCamAxisPosRot()
         {
-            if (camAxis.position != camCenter.position)
+            if (camAxis.position != camOrbitAxis.position)
             {
-                camAxis.position = camCenter.position;
+                camAxis.position = camOrbitAxis.position;
             }
 
             if (lookMode == LookMode.Forward)
@@ -113,15 +148,15 @@ namespace SolidSky
                 //}
                 //else 
                 
-                if (hoverController.camRotValueY != 0f)
-                {
-                    camAxis.Rotate(GetCameraRotation(0f, -hoverController.camRotValueY, camAxisRotDamping_C, invertGamepad, true));
-                }
+                //if (hoverController.camRotValueY != 0f)
+                //{
+                //    camAxis.Rotate(GetCameraRotation(0f, -hoverController.camRotValueY, camAxisRotDamping_C, invertGamepad, true));
+                //}
 
-                if (!lookModeTransitionActive)
-                {
-                    camAxis.eulerAngles = new Vector3(camAxis.eulerAngles.x, hoverController.transform.eulerAngles.y, 0f);
-                }
+                //if (!lookModeTransitionActive)
+                //{
+                //    camAxis.eulerAngles = new Vector3(camAxis.eulerAngles.x, hoverController.transform.eulerAngles.y, 0f);
+                //}
             }
             else if (lookMode == LookMode.Free)
             {
@@ -171,7 +206,7 @@ namespace SolidSky
             RaycastHit bumpHit;
             bumpRay = new Ray(camAxis.position, -camAxis.forward);
 
-            if (Physics.SphereCast(bumpRay, (hoverController.gameObject.transform.localScale.x/2), out bumpHit, camBumpLength, hoverPlayerLayerMask))
+            if (Physics.SphereCast(bumpRay, 2, out bumpHit, camBumpLength, hoverPlayerLayerMask))
             {
                 camPosition = new Vector3(bumpHit.point.x, bumpHit.point.y + camBumpHeight, bumpHit.point.z);
             }
--- Assets/Scripts/HoverController.cs
+++ Assets/Scripts/HoverController.cs
@@ -16,7 +16,10 @@ namespace SolidSky
             hoverInputActions = new HoverInputActions();
             hoverInputActions.PlayerHoverSmall.Enable();
             hoverInputActions.PlayerHoverSmall.Movement.performed += Movement;
-            hoverInputActions.PlayerHoverSmall.HCamera.performed += HCamera;
+
+            hoverInputActions.PlayerHoverSmall.HCamera.performed += HCameraPerformed;
+            hoverInputActions.PlayerHoverSmall.HCamera.canceled += HCameraCanceled;
+
             hoverInputActions.PlayerHoverSmall.Boost.performed += Boost;
         }
 
@@ -30,11 +33,16 @@ namespace SolidSky
             Debug.Log(context.ReadValue<float>());
         }
 
-        public void HCamera(InputAction.CallbackContext context)
+        public void HCameraPerformed(InputAction.CallbackContext context)
         {
             Debug.Log("R" + context.ReadValue<Vector2>());
             camRotValueX = context.ReadValue<Vector2>().x;
             camRotValueY = context.ReadValue<Vector2>().y;
+        }
+
+        public void HCameraCanceled(InputAction.CallbackContext context) {
+            camRotValueX = 0;
+            camRotValueY = 0;
         }
     }
 }

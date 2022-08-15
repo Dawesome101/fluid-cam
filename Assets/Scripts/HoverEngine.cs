3236849c5e5f432987c7c692501fecc1745845a1
Dawesome101 <dawesome@danawesome.com>
2022-08-14 21:36:34 -0700

    Removed dynamic addition of rigidbodies and colliders as the objects the hover engines will be attached to will be pre-set to specific types.  Setup toggleable debug visualization for hover force impacts that can activated or deactivated during runtime

--- Assets/Scripts/HoverEngine.cs
+++ Assets/Scripts/HoverEngine.cs
@@ -7,18 +7,11 @@ namespace SolidSky
     public class HoverEngine : MonoBehaviour
     {
         protected Rigidbody rb;
+        protected Collider col;
 
         public float mass;
         public float drag;
         public float force;
-
-        [Tooltip("This is the radius of the sphere used to probe for what the engine force has hit. " +
-            "The sphere will travel downward from the hover engine as far as the hover height, if it " +
-            "hits anything it will apply force in the opposite direction. If it does not hit anything, " +
-            "no force is applied. This should be equal to the extents or radius of the engines collider.  " +
-            "If one is not set, the script will calculate the size to be the same as the colliders bounds. " +
-            "The bumper ray is a sphereCast so this number should equal the radius you want. For example: " +
-            "if the collider scale is 1x 1y 1z, this should be set to 0.5")]
         public float rayBumperRadius;
 
         [Tooltip("The target height is the distance from the ground the vehicle will attempt to hover. If the" +
@@ -26,50 +19,73 @@ namespace SolidSky
         public float targetHeight;
 
         protected Ray heightRay;
-        protected RaycastHit heightRayHit;
+        private RaycastHit heightRayHit;
 
+        [Header("Debug")]
+        private GameObject debugSphere;
+        private Color castSphereColor;
+        
         public LayerMask hoverForceInteractionMask;
 
         public bool debugVisualization;
         protected void Awake()
         {
-            if (TryGetComponent(out Collider col))
-            {
-                if (col.attachedRigidbody != null)
-                {
-                    rb = col.attachedRigidbody;
-                }
-                else
-                {
-                    rb = gameObject.AddComponent<Rigidbody>();
-                    Debug.Log("A Rigidbody was added to " + transform.name + " because a Rigidbody is required for " +
-                        this + " to function. It is recommended that one be added to the object using the editor prior " +
-                        "to entering play mode.");
-                }
-            }
-            else 
-            {
-                Debug.Log("A collider was not detected on " + transform.name + ". It is strongly recommended that one " +
-                    "be manually added to the object using the editor prior to entering play mode, however a box collider " +
-                    "and rigidbody have been added to the object for the time being.");
-                gameObject.AddComponent<BoxCollider>();
-                rb = gameObject.AddComponent<Rigidbody>();
-            }
-
-            rb.mass = mass;
-            rb.drag = drag;
+            castSphereColor = new Vector4(255f, 0f, 0f, 0.5f);
         }
 
         protected void FixedUpdate()
         {
-            if (debugVisualization)
+            heightRay = new Ray(transform.position, Vector3.down);
+
+            if (debugVisualization && debugSphere == null)
             {
-                Debug.DrawLine(transform.position, -Vector3.down * targetHeight);
+                debugSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                debugSphere.transform.localScale = new Vector3(rayBumperRadius * 2, rayBumperRadius * 2, rayBumperRadius * 2);
+                Destroy(debugSphere.GetComponent<Collider>());
+                debugSphere.GetComponent<MeshRenderer>().material.color = castSphereColor;
             }
 
-            heightRay = new Ray(transform.position, -Vector3.up);
+            if (debugVisualization)
+            {
+                
+            }
 
-            //if(Physics.OverlapSphere)
+            
+
+            Collider[] overlaps = Physics.OverlapSphere(transform.position, rayBumperRadius, hoverForceInteractionMask);
+            if (overlaps.Length > 0)
+            {
+                //add force here.
+                if (debugVisualization)
+                {
+                    debugSphere.transform.position = transform.position;
+                }
+            }
+            else
+            {
+                if (Physics.Raycast(heightRay, out heightRayHit, targetHeight, hoverForceInteractionMask))
+                {
+                    //add force here.
+                    if (debugVisualization)
+                    {
+                        Debug.DrawLine(transform.position, heightRayHit.point);
+                        debugSphere.transform.position = heightRayHit.point;
+                    }
+                }
+                else
+                {
+                    if (debugVisualization)
+                    {
+                        Debug.DrawLine(transform.position, new Vector3 (transform.position.x, transform.position.y - targetHeight, transform.position.z));
+
+                        if (debugSphere != null)
+                        {
+
+                            Destroy(debugSphere);
+                        }
+                    }
+                }
+            }
         }
     }
 }
